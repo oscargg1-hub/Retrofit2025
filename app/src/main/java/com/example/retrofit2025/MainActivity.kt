125c147651b1b39e5999c425521e2979ac259d73
oscargg1-hub <oscar_gg1@tesch.edu.mx>
2025-10-14 16:29:58 -0600

    Update MainActivity.kt
    soyOscarA

--- app/src/main/java/com/example/retrofit2025/MainActivity.kt
+++ app/src/main/java/com/example/retrofit2025/MainActivity.kt
@@ -1,6 +1,6 @@
 // soy el equipo 4
 package com.example.retrofit2025
- 
+ //Soy OscarA
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
